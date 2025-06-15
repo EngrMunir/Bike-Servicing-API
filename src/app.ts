eaf090e7e7703b22bc742e2d1c6b5ec1334a5743
munir <smsirajulmonir@gmail.com>
2025-06-15 14:50:43 +0600

    basic setup with model ready

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from 'cors';
+
+const app:Application = express();
+app.use(cors());
+
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req:Request, res:Response) =>{
+    res.send({
+        Message:"Bike Servicing Server is running..."
+    })
+});
+
+
+
+export default app;
